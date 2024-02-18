a64f8e70a1715cd5fbcc539c4d61c8fa599bd6ee
Shivam Mishra <shivam552mishra@gmail.com>
2024-02-18 10:17:21 +0530

    dvr added

---
+++ .vscode/dvr.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<cmath>
+#include<climits>
+#define MAX_NODES 10
+using namespace std;
+
+void bellmanFord(int src,int nodes,int graph[MAX_NODES][MAX_NODES]){
+     int distance[MAX_NODES];
+     for(int i=0;i<nodes;i++){
+        distance[i] = INT_MAX;
+     }
+     distance[src] = 0;
+
+     for(int k=0;k<nodes-1;k++){
+        for(int i=0;i<nodes;i++){
+            for(int j=0;j<nodes;j++){
+                if(graph[i][j] != 0 && distance[i] != INT_MAX && distance[i] + graph[i][j] < distance[j]){
+                    distance[j] = distance[i] + graph[i][j]; 
+                }
+            }
+        }
+     }
+     cout<<"Vertex     distance from src:"<<endl;
+     for(int i=0;i<nodes;i++){
+        if(distance[i] == INT_MAX)
+        cout<<i<<" -> "<<"not reachable"<<endl;
+        else
+        cout<<i<<" -> "<<distance[i]<<endl;
+     }
+}
+
+int main(){
+    int nodes,graph[MAX_NODES][MAX_NODES],src;
+    cout<<"Enter the number of nodes:";
+    cin>>nodes;
+    cout<<"Enter the adjancency matrix:";
+    for(int i=0;i<nodes;i++){
+        for(int j=0;j<nodes;j++){
+            cin>>graph[i][j];
+        }
+    }
+    cout<<"Enter the source node:";
+    cin>>src;
+    bellmanFord(src,nodes,graph);
+    return 0;
+}
